b6cda7b205b812dd73768354a12d97bb56017921
Wesley Haigh <whaigh@endeavorco.com>
2019-10-14 11:30:41 +0100

    Added comment

--- src/TaglibSharp/Aac/File.cs
+++ src/TaglibSharp/Aac/File.cs
@@ -235,6 +235,11 @@ namespace TagLib.Aac
 		protected override void ReadEnd (long end, ReadStyle propertiesStyle)
 		{
 			// Make sure we have ID3v1 and ID3v2 tags.
+
+			// NOTE: these lines have been commented out to stop tags being added to the end of .aac files.
+			//		 this is a problem because ffmpeg is unable to process a stream that has an .aac file with
+			//		 a tag at the end of the file.
+			// NOTE ADDED BY DiceTechnology.
 			//GetTag (TagTypes.Id3v1, true);
 			//GetTag (TagTypes.Id3v2, true);
 		}
